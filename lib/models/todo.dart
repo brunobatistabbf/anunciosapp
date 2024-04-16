821027897f307386eb45a4c119357c63dd625b96
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2024-04-16 18:23:14 -0300

    creating database

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 class Todo {
+  int? id;
   late String texto;
   bool done = false;
   File? image;
@@ -8,15 +9,24 @@ class Todo {
   Todo(this.texto, this.image);
 
   Todo.fromMap(Map<String, dynamic> map) {
+    this.id = map['id'];
     this.texto = map['texto'];
-    this.done = map['done'];
+    this.done = map['done'] == 1 ? true : false;
+    this.image = map['imagePath'] != '' ? File(map['imagePath']) : null;
   }
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = {
+      'id': this.id,
       'texto': this.texto,
       'done': this.done,
+      'imagePath': this.image != null ? this.image!.path : ''
     };
     return map;
+  }
+
+  @override
+  String toString() {
+    return "Task(id: $id, texto: $texto, done: $done, image: $image)";
   }
 }
